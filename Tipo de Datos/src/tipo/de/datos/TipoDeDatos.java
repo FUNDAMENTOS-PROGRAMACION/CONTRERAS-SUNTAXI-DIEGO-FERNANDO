fa7d3946efcbca4a048ab9e803b1d076099e1b39
Diego Fernando C <61747581+DiegofernandoC@users.noreply.github.com>
2020-03-10 11:11:38 +0000

    tipo de datos

---
+++ Tipo de Datos/src/tipo/de/datos/TipoDeDatos.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tipo.de.datos;
+
+/**
+ *
+ * @author diego
+ */
+public class TipoDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    // enteros -32768 a 32767
+    int numeroEntero = 0;
+    double numeroDouble = 0.0;
+    char letra = 'A';
+    String cadena = "hola mundo";
+    boolean valorBool = false;
+    boolean valorBoola = true;
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+// comentario de una linea
+
+/*
+comentario con parrafo
+*/
